a900b5bbd4082c30f2041e3cd8b1a1519fc67715
PratikGavit008 <121423511+PratikGavit008@users.noreply.github.com>
2023-03-07 21:20:22 +0530

    Icon Added

--- CollectionView/CustomCell.swift
+++ CollectionView/CustomCell.swift
@@ -12,8 +12,16 @@ class CustomCell: UICollectionViewCell {
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var backView: UIView!
     override func awakeFromNib() {
-        backView.layer.borderWidth = 1
-        backView.layer.borderColor = UIColor.blue.cgColor
-        backView.layer.cornerRadius = 20
+//        backView.layer.borderWidth = 1
+//        backView.layer.borderColor = UIColor.blue.cgColor
+//        backView.layer.cornerRadius = 20
+        
+        img.layer.cornerRadius = 20
+        img.clipsToBounds = true
+        img.layer.shadowColor = UIColor.black.cgColor
+        img.layer.shadowOffset = CGSize(width: 0, height: 0)
+        img.layer.shadowOpacity = 0.5
+        img.layer.shadowRadius = 2
+        img.layer.masksToBounds = false
     }
 }
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -21,14 +21,64 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionViewHeight.constant = collectionView.frame.width / noOfCell
+        collectionViewHeight.constant = collectionView.frame.width / noOfCell + 20
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        startScrolling()
+//        startScrolling()
+        startTimer()
+        addNavBarImage()
+    }
+    func addNavBarImage() {
+            let navController = navigationController!
+            let image = UIImage(named: "logoSelfdrive") //Your logo url here
+            let imageView = UIImageView(image: image)
+//        let bannerWidth:CGFloat = 180
+            let bannerHeight = navController.navigationBar.frame.size.height - 10
+
+//        imageView.frame = CGRect(x: 0, y: 0, width: 30, height: bannerHeight)
+        imageView.contentMode = .scaleAspectFit
+        navigationItem.titleView = imageView
+      
+        
+        let imageBarButtonItem = UIBarButtonItem(customView: imageView)
+        navigationItem.leftBarButtonItem = imageBarButtonItem
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: navigationItem.leftBarButtonItem!.customView!.leadingAnchor, constant: 10),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: navController.navigationBar.frame.height - 10)
+        ])
+        
     }
     
+    
+    func startTimer() {
+        let timer =  Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.scrollAutomatically), userInfo: nil, repeats: true)
+    }
+
+
+    @objc func scrollAutomatically(_ timer1: Timer) {
+
+        if let coll  = collectionView {
+            for cell in coll.visibleCells {
+                let indexPath: IndexPath? = coll.indexPath(for: cell)
+                if ((indexPath?.row)! < arr.count - 1){
+                    let indexPath1: IndexPath?
+                    indexPath1 = IndexPath.init(row: (indexPath?.row)! + 1, section: (indexPath?.section)!)
+
+                    coll.scrollToItem(at: indexPath1!, at: .right, animated: true)
+                }
+                else{
+                    let indexPath1: IndexPath?
+                    indexPath1 = IndexPath.init(row: 0, section: (indexPath?.section)!)
+                    coll.scrollToItem(at: indexPath1!, at: .left, animated: true)
+                }
+
+            }
+        }
+        
+    }
     
     var isScrollingForward = true
     func startScrolling() {
@@ -84,6 +134,9 @@ extension ViewController : UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return spacingBetweenCells
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
     }
     
 }
